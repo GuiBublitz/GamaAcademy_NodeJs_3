87277d897157e5fa202aeacee06ac2f89032f678
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-10-29 12:08:14 -0300

    Atualizando para o caso da energia elétrica resolva cair

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-//Biblioteca
-const rl = require('./interface/index');
+// //Biblioteca
+// const rl = require('./interface/index');
 
-rl.question('Are you enjoying the OOP class?', answer =>{
-    console.log(`Did you answer that? : ${answer}`);
-    rl.close();
-});
+// rl.question('Are you enjoying the OOP class?', answer =>{
+//     console.log(`Did you answer that? : ${answer}`);
+//     rl.close();
+// });
+const options = require('./data/index');
+const MachineOptions = require('./controller/class/index');
+
+let random = new MachineOptions({ opt: options }).sort();
+
+console.log(random);
