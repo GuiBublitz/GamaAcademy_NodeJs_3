414c87a98f0bdc529d1b2205a86f6deaa820305d
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-10-29 15:43:31 -0300

    Controller machine

--- src/controller/class/index.js
+++ src/controller/class/index.js
@@ -3,15 +3,15 @@ class MachineOptions{
         this._opt = opt;
     }
     sort(){
-        const sortOpt = Math.random() * 3 - 0;
+        const sortOpt = Math.floor(Math.random() * (3 - 0));
         return this._opt[sortOpt].name;
     }
-    set opt(option){
-        this._opt = option;
-    }
-    get opt(){
-        return this._opt;
-    }
+    // set opt(option){
+    //     this._opt = option;
+    // }
+    // get opt(){
+    //     return this._opt;
+    // }
 }
 
 module.exports = MachineOptions;
