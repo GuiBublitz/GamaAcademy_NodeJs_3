718f44d5ae27cca39320ba0ff2a9c7a7722c07df
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-11-03 10:16:23 -0300

    Att libs/interface inquirer

--- src/controller/class/user.js
+++ src/controller/class/user.js
@@ -1,4 +1,6 @@
 const MachineOptions = require('./machine');
+const inquirer = require('inquirer');
+const options = require('../../data/index');
 
 class User extends MachineOptions {
     constructor({ opt, name, selected }) {
@@ -23,16 +25,33 @@ class User extends MachineOptions {
     logic() {
         if (this._selected === this._sort) {
             return `uhhhhhhhhhh this's a draw ${this._name}!!! ${this._name}: ${this._selected} | Machine: ${this._sort}`;
-        } else if ((this._selected === "Rock" && this._sort === "Scissors") || (this._selected === "Paper" && this._sort === "Rock") || (this._selected === "Scissors" && this._sort === "Paper") ) {
+        } else if ((this._selected === "Rock" && this._sort === "Scissors") || (this._selected === "Paper" && this._sort === "Rock") || (this._selected === "Scissors" && this._sort === "Paper")) {
             return `Uowww ${this._name} WINSS! ${this._name}: ${this._selected} | Machine: ${this._sort}`;
-        } 
+        }
         return `Opss ${this._name} Loses! ${this._name}: ${this._selected} | Machine: ${this._sort}`;
-    
+
     }
     game() {
-        return this.logic();
+        return inquirer
+            .prompt([
+                /* Pass your questions in here */
+                {
+                    name: 'name',
+                    message: 'Qual seu nome?',
+                    default : 'Jogador'
+                },
+                {
+                    type: 'list',
+                    name: 'jokenpo',
+                    message: 'Selecione uma destas opções',
+                    choices: options
+                }
+            ]).then((answers) => {
+                this._name = answers.name;
+                this._selected = answers.jokenpo;
+                console.info(this.logic());
+            })
     }
 
 }
-
 module.exports = User;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// //Biblioteca
+// Biblioteca
 // const rl = require('./interface/index');
 
 // rl.question('Are you enjoying the OOP class?', answer =>{
